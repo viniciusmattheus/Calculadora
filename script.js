406ffc2fa88fa47f3976e3c040f9f8ff6ec7c31d
Vinicius Matheus <viniciusmattheus@outlook.com>
2022-06-09 21:17:45 -0300

    Update

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const resultado = document.querySelector(' .result ');
+const resultado = document.querySelector(' .result '); 
 const confirmar = document.qyerySelector(' .igual ');
 
+//Função para inserir os números no visor
 function insert( valor ) {
     resultado.innerHTML += valor;
 }
 
+//Função para limpar o visor.
 function clean() {
     resultado.innerHTML = ' ';
 }
 
+//Função para limpar o último número do visor.
 function backspace() {
     if(resultado.textContent) {
         let result = document.getElementById('resultado').innerHTML
@@ -16,6 +19,7 @@ function backspace() {
     }
 }
 
+//Função para calcular as operações.
 function confirma() {
     if(resultado.textContent != 'Erro') {
         document.getElementById("resultado").innerHTML = eval(resultado.innerHTML);
